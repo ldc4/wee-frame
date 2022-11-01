df44fbcd1a1c1dd03149877330bc69b517c86f38
weedust <ldc4@vip.qq.com>
2022-11-01 19:30:51 +0800

    chore: 调整目录结构,采用lerna多包管理

--- src/component/Layout.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function Layout() {
-  return <div className="hello">hello wee-frame</div>
-}
-
-export default Layout;
--- src/index.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import MainRouter from './component/MainRouter';
-import './index.less';
-
-ReactDOM.render(<MainRouter />, document.getElementById('root'));
